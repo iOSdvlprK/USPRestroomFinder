4b2a77370eccc7fa35ea5b2305515a9df8f5b692
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-12-28 21:15:16 +0900

    Initial Commit

---
+++ USPRestroomFinder/USPRestroomFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  USPRestroomFinderApp.swift
+//  USPRestroomFinder
+//
+//  Created by joe on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct USPRestroomFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
